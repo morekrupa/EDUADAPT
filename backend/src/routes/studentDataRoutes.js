84c0bd489e3fe4b6716e480173eae27187e63959
meghamcr <157197202+meghamcr@users.noreply.github.com>
2026-08-20 23:34:19 +0530

    feat: expose student mastery and recommendations

--- backend/src/routes/studentDataRoutes.js
+++ backend/src/routes/studentDataRoutes.js
@@ -5,5 +5,8 @@ const studentController = require('../controllers/studentController');
 
 router.get('/:id/sessions', verifyToken, studentController.getSessions);
 router.get('/:id/progress', verifyToken, studentController.getProgress);
+router.get('/me/progress', verifyToken, (req, res, next) => { req.params.id = req.user.userId; next(); }, studentController.getProgress);
+router.get('/me/mastery', verifyToken, studentController.getMastery);
+router.get('/me/recommendations', verifyToken, studentController.getRecommendations);
 
 module.exports = router;
